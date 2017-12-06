3c97fa9464bab91739596b12f42d208af2cac6b7
Andrew <andrew.j.zeller@gmail.com>
2017-12-06 00:15:26 -0800

    add what is it and how it works sections to home page

--- js/scroll_fade_in.js
+++ js/scroll_fade_in.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
 
 // init controller
-	// var controller = new ScrollMagic.Controller();
+	var controller = new ScrollMagic.Controller();
 
 	// build scene
-	// var scene = new ScrollMagic.Scene({triggerElement: "#trigger"})
+	var scene = new ScrollMagic.Scene({
+						triggerElement: "#trigger"
+					})
+					.setTween("#animate1", 0.5, {backgroundColor: "green", scale: 2.5}) // trigger a TweenMax.to tween
+					.addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
+					.addTo(controller);
 					// trigger a velocity opaticy animation
 					// $("#animate")
 					// 	.velocity("slideDown", { duration: 1500 })
@@ -14,51 +19,5 @@ $(document).ready(function(){
 					// .setVelocity("#animate", {opacity: 100}, {duration: 400})
 					// .addIndicators() // add indicators (requires plugin)
 					// .addTo(controller);
-
-var theme_bg_color = "#0074D9";
-var fade_bg_color = "#4AABFF";
-
-function bg_fade(){
-	$(".container--gradient-bg").velocity({
-		/* Animate a color property to a hex value of red... */
-		backgroundColor: theme_bg_color,
-		/* ... with a 50% opacity. */
-		backgroundColorAlpha: 1,
-		/* Animate the red RGB component to 50% (0.5 * 255). */
-		// colorRed: "50%",
-		// /* Concurrently animate to a stronger blue. */
-		// colorBlue: "+=50",
-		// /* Fade the text down to 85% opacity. */
-		// colorAlpha: 0.8
-	}, 5000 );
-
-	$(".container--gradient-bg").velocity({
-		/* Animate a color property to a hex value of red... */
-		backgroundColor: fade_bg_color,
-		/* ... with a 50% opacity. */
-		backgroundColorAlpha: 1,
-		/* Animate the red RGB component to 50% (0.5 * 255). */
-		// colorRed: "50%",
-		// /* Concurrently animate to a stronger blue. */
-		// colorBlue: "+=50",
-		// /* Fade the text down to 85% opacity. */
-		// colorAlpha: 0.8
-	}, 5000 );
-
-	$(".container--gradient-bg").velocity({
-		/* Animate a color property to a hex value of red... */
-		backgroundColor: theme_bg_color,
-		/* ... with a 50% opacity. */
-		backgroundColorAlpha: 1,
-		/* Animate the red RGB component to 50% (0.5 * 255). */
-		// colorRed: "50%",
-		// /* Concurrently animate to a stronger blue. */
-		// colorBlue: "+=50",
-		// /* Fade the text down to 85% opacity. */
-		// colorAlpha: 0.8
-}, 5000 );
-}
-
-setInterval(bg_fade, 500);
 
 });
